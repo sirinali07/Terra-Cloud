4b375659596ae9fe2a79fcd048ae67a0c29eb802
Sirin Ali <118505493+sirinali07@users.noreply.github.com>
2025-12-06 19:00:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+resource "azurerm_resource_group" "RG" {
+  name     = "Azure_Tf_Resource-_Group"
+  location = "East US"
+}
+
+ provider "azurerm" {
+  features {}
+
+  skip_provider_registration = "true"
+
+  # Connection to Azure
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
